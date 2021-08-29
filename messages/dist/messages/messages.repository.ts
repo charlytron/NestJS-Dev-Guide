d9d3d94bf32e30cf93e26a2574224d449b53c7b8
C Tompkins <celloviolinbass@yahoo.com>
2021-08-29 14:33:56 -0500

    implementing a service

---
+++ messages/dist/messages/messages.repository.ts
@@ -0,0 +1,30 @@
+import { readFile, writeFile } from 'fs/promises'
+import { floor } from '../../../../../charlestompkins/Library/Caches/typescript/4.3/node_modules/@types/lodash';
+
+export class MessagesRepository {
+  // async here bec we need to read a file from the HD
+  async findOne (id: string) {
+    const contents = await readFile('messages.json', 'utf8')
+    const messages = JSON.parse(contents)
+
+    return messages[id]
+  }
+
+  async findAll () {
+    const contents = await readFile('messages.json', 'utf8')
+    const messages = JSON.parse(contents)
+
+    return messages
+  }
+
+  async create (content: string) {
+    const contents = await readFile('messages.json', 'utf8');
+    const messages = JSON.parse(contents);
+
+    const id = Math.floor(Math.random() * 999);
+
+    messages[id] = { id, content };
+
+    await writeFile('messages.json', JSON.stringify(messages));
+  }
+}
